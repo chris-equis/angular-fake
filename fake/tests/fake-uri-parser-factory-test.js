ee4ed32e7ec1ec7f7c4d029b4a506a41193f8729
Cristi Socea <office@soceacristian.ro>
2015-09-07 20:58:00 +0300

    Update FakeUriParser factory tests

--- fake/tests/fake-uri-parser-factory-test.js
+++ fake/tests/fake-uri-parser-factory-test.js
@@ -4,38 +4,65 @@ describe('Testing FakeUriParser factory', function() {
       paramRegExpString = '([a-z0-9\\-\\_]+)',
       queryRegExpString = '(\\?.*)?';
 
-  beforeEach(module('fake'));
+  beforeEach(function() {
+    angular
+    .module('test', ['fake'])
+    .config(['FakeConfigProvider', function(FakeConfig) {
+      FakeConfig.path('root', 'http://api.example.com:8080/v1');
+    }]);
+    module('test');
+  });
 
   beforeEach(inject(function(_FakeUriParser_) {
     FakeUriParser = _FakeUriParser_;
+
     parsers['libraries'] = new FakeUriParser('/libraries');
     parsers['library'] = new FakeUriParser('/libraries/{libraryId}');
     parsers['books'] = new FakeUriParser('/libraries/{libraryId}/books');
     parsers['book'] = new FakeUriParser('/libraries/{libraryId}/books/{bookId}');
+    parsers['user'] = new FakeUriParser('$root/users/{uid}');
+
+    // @TODO: Test some invalid parsers like '/users/{}/profiles'
   }));
 
-
   it('Should extract path param names/keys', function() {
-    expect(parsers.library.params).toEqual(['libraryId']);
-    expect(parsers.books.params).toEqual(['libraryId']);
-    expect(parsers.book.params).toEqual(['libraryId', 'bookId']);
+    var tests = [
+      { parser: 'library', params: ['libraryId'] },
+      { parser: 'books', params: ['libraryId'] },
+      { parser: 'book', params: ['libraryId', 'bookId'] }
+    ];
+
+    tests.forEach(function(test) {
+      expect(parsers[test.parser].params).toEqual(test.params);
+    });
   });
 
-  it('Should created a pattern', function() {
-    var libraryRegExp = new RegExp(
-          '^\\/libraries\\/' + paramRegExpString + queryRegExpString + '$'),
+  it('Should match URLs', function() {
+    var tests = [
+      { parser: 'libraries', url: '/libraries' },
+      { parser: 'libraries', url: '/libraries?city=London' },
+      { parser: 'library', url: '/libraries/1' },
+      { parser: 'library', url: '/libraries/1?fields=name&' },
+      { parser: 'books', url: '/libraries/1/books?author=John%20Doe' },
+      { parser: 'user', url: 'http://api.example.com:8080/v1/users/1' }
+    ];
 
-        booksRegExp = new RegExp(
-          '^\\/libraries\\/' + paramRegExpString +
-          '\\/books' + queryRegExpString + '$'),
+    tests.forEach(function(test) {
+      expect(parsers[test.parser].pattern.test(test.url)).toBe(true);
+    });
+  });
 
-        bookRegExp = new RegExp(
-          '^\\/libraries\\/' + paramRegExpString +
-          '\\/books\\/' + paramRegExpString + queryRegExpString + '$');
+  it('Should NOT match URLs', function() {
+    var tests = [
+      { parser: 'libraries', url: '/libraries/1' },
+      { parser: 'books', url: '/libraries/1/books/2?author=John%20Doe' },
+      { parser: 'user', url: 'http://api.example.com:8080/v1/users' },
+      { parser: 'user', url: 'http://api.example.com:9000/v1/users/1' }
+    ];
 
-    expect(parsers.library.pattern).toEqual(libraryRegExp);
-    expect(parsers.books.pattern).toEqual(booksRegExp);
-    expect(parsers.book.pattern).toEqual(bookRegExp);
+    tests.forEach(function(test) {
+      expect(parsers[test.parser].pattern.test(test.url)).toBe(false);
+    });
   });
 
   it('Should get path params after parsing URI', function() {
@@ -44,7 +71,8 @@ describe('Testing FakeUriParser factory', function() {
       { parser: 'libraries', url: '/libraries', expected: {} },
       { parser: 'library', url: '/libraries/1', expected: { libraryId: '1' } },
       { parser: 'books', url: '/libraries/1/books', expected: { libraryId: '1' } },
-      { parser: 'book', url: '/libraries/1/books/2?test=query', expected: { libraryId: '1', bookId: '2' } }
+      { parser: 'book', url: '/libraries/1/books/2?test=query', expected: { libraryId: '1', bookId: '2' } },
+      { parser: 'user', url: 'http://api.example.com:8080/v1/users/1', expected: { uid: '1' } },
     ];
 
     tests.forEach(function(test) {
